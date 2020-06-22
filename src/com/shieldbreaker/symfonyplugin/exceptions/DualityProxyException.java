eea3c5f53a54fdc05dec5e036d70e01ed0f523f7
PandatiX <lucasfloriantesson@gmail.com>
2020-06-22 23:53:16 +0200

    Removed code & libs due to WebProxyCore & WebBruteForceCore build => Incremented version

--- src/com/shieldbreaker/symfonyplugin/exceptions/DualityProxyException.java
+++
@@ -1,8 +0,0 @@
-package com.shieldbreaker.symfonyplugin.exceptions;
-
-public class DualityProxyException extends Exception {
-    @Override
-    public String getMessage() {
-        return "SOCKS and HTTP proxies cannot be set at same time. Please check https://bitrebels.com/technology/difference-between-socks-proxy-http-proxy/";
-    }
-}
